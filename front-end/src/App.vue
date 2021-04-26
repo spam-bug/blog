ae9fa7fddab58171c62bcf0b7ee17e5240cf8999
Raymark Peña <dev.mlimp@gmail.com>
2021-04-26 18:54:24 +0800

    Initial Set up
    Deleting HelloWorld component, modifying routes, and deleting about view

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+html {
+  scroll-behavior: smooth;
+  font-size: 62.50%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  font-family: sans-serif;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+a {
+  text-decoration: none;
 }
+
+ul {
+  list-style-type: none;
+}
+
 </style>
