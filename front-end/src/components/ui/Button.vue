8e7d1d786967c2bcc06a1d3a4f7846ae82ee3961
Raymark Peña <dev.mlimp@gmail.com>
2021-04-28 14:15:20 +0800

    Create button ui

---
+++ front-end/src/components/ui/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button :class="['btn', 'btn-' + type]"><slot/></button>
+</template>
+
+<script>
+export default {
+  name: "button",
+  props: {
+    type: String
+  }
+}
+</script>
+
+<style scoped>
+.btn {
+  padding: .7em 2em;
+  min-width: 120px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn-default {
+  color: #707070;
+  background: white;
+  border-color: #C4C4C4;
+}
+
+.btn-default:active {
+  color: white;
+  background: #15B1F6;
+  border-color: #15B1F6;
+}
+</style>
