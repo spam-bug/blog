c142d607d52e8a6a6e037be745739d7dfae4cf7f
Raymark Peña <dev.mlimp@gmail.com>
2021-04-29 17:56:04 +0800

    Add constant action

---
+++ front-end/src/store/action.type.js
@@ -0,0 +1,47 @@
+export const USER_CREATE = "createUser";
+export const USER_EDIT = "editUser";
+export const USER_DELETE = "deleteUser";
+export const USER_ADD_ROLE = "addRoleToUser";
+export const USER_REMOVE_ROLE = "removeRoleFromUser";
+
+export const ROLE_CREATE = "createRole";
+export const ROLE_EDIT = "editRole";
+export const ROLE_DELETE = "deleteRole";
+export const ROLE_ADD_PERMISSION = "addPermissionToRole";
+export const ROLE_REMOVE_PERMISSION = "removePermissionFromRole";
+
+export const BLOG_PUBLISH = "publishBlog";
+export const BLOG_EDIT = "editBlog";
+export const BLOG_DELETE = "deleteBlog";
+export const BLOG_ADD_TAG = "addTagToBlog";
+export const BLOG_REMOVE_TAG = "removeTagFromBlog";
+export const BLOG_EDIT_STATUS = "editBlogStatus";
+
+export const DRAFT_PUBLISH = "publishDraft";
+export const DRAFT_EDIT = "editDraft";
+export const DRAFT_DELETE = "deleteDraft";
+
+export const TAG_CREATE = "createTag";
+export const TAG_EDIT = "editTag";
+export const TAG_DELETE = "deleteTag";
+
+export const NOTIFICATION_SEND = "sendNotification";
+export const NOTIFICATION_EDIT_STATUS = "editNotificationStatus";
+export const NOTIFICATION_DELETE = "deleteNotification";
+
+export const FETCH_BLOG = "fetchBlog";
+export const FETCH_BLOGS = "fetchBlogs";
+export const FETCH_TRASH = "fetchTrash";
+export const FETCH_TRASH_ALL ="fetchAllTrash";
+export const FETCH_USER = "fetchUser";
+export const FETCH_USER_ACTIVE = "fetchActiveUser";
+export const FETCH_ROLE = "fetchRole";
+export const FETCH_ROLES = "fetchRoles";
+export const FETCH_NOTIFICATION = "fetchNotification";
+export const FETCH_NOTIFICATIONS = "fetchNotifications";
+export const FETCH_TAG = "fetchTag";
+export const FETCH_TAGS = "fetchTags";
+
+export const LOGIN = "login";
+export const LOGOUT = "logout";
+export const CHECK_AUTH = "checkAuth";
