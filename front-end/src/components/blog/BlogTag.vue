b5c788225e467fa376c5e8948d8589696f055943
Raymark Peña <dev.mlimp@gmail.com>
2021-04-29 17:58:34 +0800

    Create BlogTag component

---
+++ front-end/src/components/blog/BlogTag.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <AppLabel label="Tags"/>
+    <BlogTagList/>
+  </div>
+</template>
+
+<script>
+import AppLabel from "../ui/AppLabel";
+import BlogTagList from "./BlogTagList";
+
+export default {
+  name: "BlogTag",
+  components: {
+    AppLabel,
+    BlogTagList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
