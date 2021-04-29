9a506157942c793dd1cfb9321feb67fe4e8d10b2
Raymark Peña <dev.mlimp@gmail.com>
2021-04-29 17:57:08 +0800

    Create AppLabel component

---
+++ front-end/src/components/ui/AppLabel.vue
@@ -0,0 +1,20 @@
+<template>
+  <label>{{ (label) ? label : 'Label' }}</label>
+</template>
+
+<script>
+export default {
+  name: "AppLabel",
+  props: {
+    label: String
+  }
+}
+</script>
+
+<style scoped>
+label {
+  display: block;
+  font-size: 1.4rem;
+  font-weight: 600;
+}
+</style>
